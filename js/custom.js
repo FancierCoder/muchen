637de6bdb9cc95f49db062d237f3ac5a1495bb7d
FancierCoder <lcoder@qq.com>
2020-05-21 22:42:08 +0800

    Blog Update

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,1 @@
-//点击事件
-function switchDarkMode() {
-  if ($('body').hasClass('dark')) {
-    $("#dark").html("☀");
-    document.body.classList.remove('dark');
-    localStorage.setItem('noDark', '1');
-    localStorage.setItem('dark', '0');
-  } else {
-    $("#dark").html("🌙");
-    document.body.classList.add('dark');
-    localStorage.setItem('dark', '1');
-    localStorage.setItem('noDark', '0');
-  }
-}
+function switchDarkMode(){$("body").hasClass("dark")?($("#dark").html("☀"),document.body.classList.remove("dark"),localStorage.setItem("noDark","1"),localStorage.setItem("dark","0")):($("#dark").html("🌙"),document.body.classList.add("dark"),localStorage.setItem("dark","1"),localStorage.setItem("noDark","0"))}
